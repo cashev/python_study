72d179413f434dd729d19ce42f090b58b918ee3a
cashew <42924021+cashev@users.noreply.github.com>
2021-01-01 15:01:56 +0900

    ロジスティック写像の可視化

--- ch03/logistic.py
+++ ch03/logistic.py
@@ -1,3 +1,5 @@
+import matplotlib as mpl
+mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 
 def logistic(r):
@@ -7,8 +9,20 @@ def logistic(r):
     if i > 990:
       print(n)
 
-logistic(1.5)
-print()
-logistic(2.0)
-print()
-logistic(3.1)
+def logistic_plot(start, end, x, y):
+  for i in range(1000):
+    r = (end - start) * i / 1000 + start
+    n = 0.1
+    for j in range(1000):
+      n = r * n * (1.0 - n)
+      if j > 900:
+        x.append(r)
+        y.append(n)
+
+def plot(start, end):
+  x, y = [], []
+  logistic_plot(start, end, x, y)
+  plt.scatter(x, y, s=0.1)
+  plt.show()
+
+plot(1.0, 3.0)
