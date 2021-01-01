9828da6172ffaf3f95af074cea22c25b00529b4a
cashew <42924021+cashev@users.noreply.github.com>
2021-01-01 16:16:21 +0900

    コラッツ数列のグラフを表示

--- ch03/collatz.py
+++ ch03/collatz.py
@@ -1,4 +1,4 @@
-# from graphviz import Digraph
+from graphviz import Digraph
 
 def collatz(i):
   print(i)
@@ -9,4 +9,23 @@ def collatz(i):
       i = i * 3 + 1
     print(i)
 
-collatz(3)
+def collatz_graph(i, edges):
+  while i != 1:
+    j = i
+    if i % 2 == 0:
+      i = i // 2
+    else:
+      i = i * 3 + 1
+    edges.add((j, i))
+
+def make_graph(n):
+  g = Digraph()
+  edges = set()
+  for i in range(1, n+1):
+    collatz_graph(i, edges)
+  for i, j in edges:
+    g.edge(str(i), str(j))
+  g.attr(size="10,10")
+  return g
+
+make_graph(3)
